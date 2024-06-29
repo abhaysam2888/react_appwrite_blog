7ccd77cbaf77f0fb065f4d8c06f39a0fbad2e91f
abhaysam2888 <abhaysam2888@gmail.com>
2024-06-30 02:02:38 +0530

    rte setteled

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -34,14 +34,38 @@ function RTE({name, control, label, defaultValue =""}) {
                     "insertdatetime",
                     "media",
                     "table",
-                    "code",
                     "help",
                     "wordcount",
                     "anchor",
                 ],
                 toolbar:
                 "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px;}"
+
+                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px;}",
+
+                mobile: {
+                    menubar: true,
+                    plugins: [
+                      "advlist",
+                    "autolink",
+                    "lists",
+                    "link",
+                    "charmap",
+                    "preview",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "help",
+                    "wordcount",
+                    "anchor",
+                    ],
+                    toolbar: 'undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help'
+                  }
             }}
             onEditorChange={onChange}
             />
